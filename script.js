95c97a066434be7b312312542604618992805893
nicol-hawkins <nicolchawkins@gmail.com>
2020-07-08 17:08:39 -0700

    committing changed to check out previous branch

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var myNewlist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNewlist.length; i++) {
-  var span = document.createElement("span");
+  var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
@@ -22,29 +22,30 @@ for (i = 0; i < close.length; i++) {
 
 // Create a new list item
 function newElement() {
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("listInput").value;
-  const t = document.createTextNode(inputValue);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("listInput").value;
+  var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You didn't add anything!");
   } else {
     document.getElementById("myItems").appendChild(li);
   }
+
   //when uncommented, 'x' button dissapears from new list item...
-  document.getElementById("listInput").value = "";
+  // document.getElementById("listItem").value = "";
 
   //Adds close button to newly spanned list items
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
 
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
   }
+}
